0f0536d21db3195242a0648667baa250ac262247
Thiru <pulgamthiru69@gmail.com>
2025-02-12 11:22:58 +0530

    developed bye feature

--- GitHubProject/src/com/Test.java
+++ GitHubProject/src/com/Test.java
@@ -3,7 +3,11 @@ package com;
 public class Test {
 
 	public static void main(String[] args) {
-     System.out.println("i am a manager...");
+		System.out.println("i am a manager...");
+	}
+
+	public static void sayBye() {
+		System.out.println("bye this is developer 2..");
 	}
 
 	public static void sayhi() {
